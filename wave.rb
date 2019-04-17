3cad82b71d2e67951cc04b271c78942edb6e702c
github username <github email address>
2019-04-17 14:54:40 -0400

    done a few things

--- wave.rb
+++ wave.rb
@@ -6,8 +6,15 @@ def solution(s)
   first = newstring.slice(0..2)
   second = newstring.slice(3..5)
   third = newstring.slice(6..8)
-  fourth = newstring.slice(9..10)
-  first + "-" + second + "-" + third + "-" + fourth
+  fourth = newstring.slice(9..11)
+  fifth = newstring.slice(12..14)
+  number = first + "-" + second + "-" + third + "-" + fourth + "-" + fifth
+  if number[-1, 1] == "-"
+    number.slice(number.length - 2)
+  else
+    number
+  end
+
 end
 
 #I took out any weird characters and then sliced it up into groups of 3, then chomped off any dashes off the end
